dc4c0045263652ab35da0a91277db38ecc4d3f3c
Сергей Лакас <serhiy.lakas@hotmail.com>
2025-11-13 08:23:07 +0200

    Remove libraries Sabatex.Core and Sabatex.Extensions

--- Sabatex.Core/IEntityReference.cs
+++
@@ -1,9 +0,0 @@
-namespace Sabatex.Core;
-/// <summary>
-/// 
-/// </summary>
-/// <typeparam name="TKey"></typeparam>
-public interface IEntityReference<TKey>:IEntityBase<TKey>,IDeletionMark
-{
-
-}
