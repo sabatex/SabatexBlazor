aa765da7e3a93ce3a1e31937f5cd74ebe40f67bc
Serhiy Lakas <serhiy.lakas@hotmail.com>
2025-12-12 14:41:59 +0200

    Remove Blazor WASM demo app and all related assets
    This commit deletes the entire Blazor WebAssembly demo application, including all source code, components, models, services, configuration files, stylesheets, static assets (such as icons and manifest), documentation, and service worker scripts. The Bootstrap CSS source map file is also removed. The project no longer contains any files necessary to build, run, or describe the demo app.

--- Sabatex.RadzenBlazor.Demo/Models/Person.cs
+++
@@ -1,9 +0,0 @@
-﻿using Sabatex.Core;
-
-namespace Sabatex.RadzenBlazor.Demo.Models;
-
-public class Person:IEntityBase<Guid>
-{
-    public string Name { get; set; }
-    public Guid Id {get; set; }
-}
